0713f00caf7668851c097ff366e4ce8ff3c2fdbf
Victor Manuel Hdz <victortomas567@gmail.com>
2025-11-26 17:05:44 -0600

    Add files via upload
    archivos faltantes

---
+++ Program.cs
@@ -0,0 +1,24 @@
+namespace pryCafeteriaUTHH
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+
+            using (frmLogin login = new frmLogin())
+            {
+                if (login.ShowDialog() == DialogResult.OK)
+                {
+                    Application.Run(new frmPrincipal());
+                }
+            }
+        }
+    }
+}
